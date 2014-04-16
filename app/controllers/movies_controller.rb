4faf089775226470ed552cf122c792266caa6102
Lucian Boboc <info@lucianboboc.com>
2014-04-17 01:32:04 +0300

    movies controller changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
 	def index
 		@movies = Movie.all
 	end
+
+	def show
+		@movie = Movie.find_by(:id => params[:id])
+		Rails.logger.debug(@movie)
+	end
 end
