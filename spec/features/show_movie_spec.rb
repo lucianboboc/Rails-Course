ba022873b1aa416c3ea54cc0dac9bf2ed808d92a
Lucian Boboc <info@lucianboboc.com>
2014-04-19 00:41:08 +0300

    show movie spec

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -4,18 +4,31 @@ describe "Viewing an individual movie" do
 	
 	it "shows the movie's details" do
 
-		movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+		movie = Movie.create(movie_attributes(total_gross: 300000000.00))
 
 		visit "http://example.com/movies/#{movie.id}"
 
 		expect(page).to have_text(movie.title)
 		expect(page).to have_text(movie.rating)
-		expect(page).to have_text("$318,412,101.00")
+		expect(page).to have_text("$300,000,000.00")
 		expect(page).to have_text(movie.description)
 		expect(page).to have_text(movie.released_on)
 	end
+
+	it "shows the total gross if the total gross exceeds $50M" do
+		movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+
+		visit movie_path(movie)
+
+		expect(page).to have_text("$60,000,000.00")
+	end
+
+	it "shows 'Flop!' if the total gross is less than $50M" do
+		movie = Movie.create(movie_attributes(total_gross: 30000000.00))
+
+		visit movie_path(movie)
+
+		expect(page).to have_text("Flop!")
+	end
+
 end
