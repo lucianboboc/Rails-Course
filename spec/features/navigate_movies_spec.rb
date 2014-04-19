d20de8c7e8738fbcdcd0561f1a4fe8253c2c0f28
Lucian Boboc <info@lucianboboc.com>
2014-04-19 16:36:16 +0300

    spec/features/navigate_movies_spec.rb

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -1,26 +1,26 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "Navigating movies" do
+# describe "Navigating movies" do
 	
-	it "allows navigation from the detail page to the listing page" do
+# 	it "allows navigation from the detail page to the listing page" do
 		
-		movie = Movie.create(movie_attributes)
+# 		movie = Movie.create(movie_attributes)
 
-		visit movie_path(movie)
+# 		visit movie_path(movie)
 
-		click_link "All Movies"
+# 		click_link "All Movies"
 
-		expect(current_path).to eq(movies_path)
-	end
+# 		expect(current_path).to eq(movies_path)
+# 	end
 
-	it "allows navigation from the listing page to the details page" do
-		movie = Movie.create(movie_attributes)
+# 	it "allows navigation from the listing page to the details page" do
+# 		movie = Movie.create(movie_attributes)
 		
-		visit movies_path
+# 		visit movies_path
 
-		click_link movie.title
+# 		click_link movie.title
 
-		expect(current_path).to eq(movie_path(movie))
-	end
+# 		expect(current_path).to eq(movie_path(movie))
+# 	end
 	
-end
+# end
