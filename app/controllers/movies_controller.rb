401f956afb27be32883e2400b1e05fe45f1b333a
Lucian Boboc <info@lucianboboc.com>
2014-04-24 02:22:23 +0300

    edit and update methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,20 @@ class MoviesController < ApplicationController
 		@movie = Movie.find_by(:id => params[:id])
 		Rails.logger.debug(@movie)
 	end
+
+	def edit
+		@movie = Movie.find_by(:id => params[:id])
+	end
+
+	def update
+		@movie = Movie.find_by(:id => params[:id])
+		@movie.update_attributes(save_movie_params)
+		
+		redirect_to movie_path(@movie)
+	end
+
+	private
+	def save_movie_params
+		params.require(:movie).permit(:title,:rating,:total_gross,:description,:released_on)
+	end
 end
