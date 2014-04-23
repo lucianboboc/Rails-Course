c522fa97d2340f33350e8dcd4ec5bc821b435339
Lucian Boboc <info@lucianboboc.com>
2014-04-24 02:23:49 +0300

    routes configured for edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Flix::Application.routes.draw do
 
-  resources :movies, only: [:index,:show]
-  # get "/movies" => "movies#index"
-  # get "movies/:id" => "movies#show"
+  # resources :movies, only: [:index,:show]
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show", as: :movie
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+  patch "/movies/:id" => "movies#update"
 
   # resources :movies
 
