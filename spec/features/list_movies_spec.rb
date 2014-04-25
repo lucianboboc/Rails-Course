dde71a9b6d8172e7f613e0615b1d785c66b8add6
Lucian Boboc <info@lucianboboc.com>
2014-04-25 12:59:33 +0300

    list movies spec changes

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe "View the list of movies" do
-	it "show the movies" do
+	
+	movie1 = nil
+	movie2 = nil
+	movie3 = nil
+	movie4 = nil
 
+	before :each do
 		movie1 = Movie.create(title: "Iron Man",
 		                      rating: "PG-13",
 		                      total_gross: 318412101.00,
@@ -26,6 +31,16 @@ describe "View the list of movies" do
 							    total_gross: 40200000.00,
 							    description: "Patience Philips has a more than respectable career as a graphic designer",
 							    released_on: "2004-07-23")
+	end
+
+	after :each do
+		movie1 = nil
+		movie2 = nil
+		movie3 = nil
+		movie4 = nil
+	end
+
+	it "show the movies" do
 
 		visit movies_url
 
@@ -36,5 +51,15 @@ describe "View the list of movies" do
 		expect(page).to have_text(movie4.title)
 
 		expect(page).to have_text(movie1.rating)
+	end
+
+
+	it "does not show a movie that hasn't yet been released" do
+		
+		movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+		visit movies_path
+
+		expect(page).not_to have_text(movie.title)
 	end
 end
