d024937d5263bed79387c37e6b2a757430d59880
Lucian Boboc <info@lucianboboc.com>
2014-05-19 00:57:32 +0300

    app/models/movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,14 @@
 class Movie < ActiveRecord::Base
 
+	validates :title, :released_on, :duration, presence: true
+	validates :description, :presence => true, :length => {:minimum => 30}
+	validates :total_gross, :numericality => {:greater_than_or_equal_to => 0}
+	validates :image_file_name, allow_blank: true, format: {with: /\w+.(gif|jpg|png)\z/i , message: "must reference a GIF, JPG, or PNG image"}
+	
+	RATINGS = %w(G PG PG-13 R NC-17)
+	validates :rating, inclusion: {in: RATINGS}
+
+
 	def flop?
 		total_gross.blank? || total_gross < 50000000
 	end
