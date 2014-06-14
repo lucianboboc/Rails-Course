619928f99bb1d9579d40057c82bfffac111f272e
Lucian Boboc <info@lucianboboc.com>
2014-06-14 21:30:54 +0300

    reviews controller index, new, create + private methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,26 @@
 class ReviewsController < ApplicationController
-	
+	def index
+		@movie = Movie.where(:id => params[:movie_id]).first
+		# @reviews = @movie.reviews
+		@reviews = Review.where(movie_id: @movie.id)
+	end
+
+	def new
+		@movie = Movie.where(:id => params[:movie_id]).first
+		@review = @movie.reviews.new
+	end
+
+	def create
+		@movie = Movie.where(:id => params[:movie_id]).first
+		@review = @movie.reviews.new(review_params)
+		if @review.save
+			redirect_to movie_reviews_path(@movie), :notice => "Thanks for the review!"
+		else
+			render :new
+		end
+	end
+
+	def review_params
+		params.require(:review).permit(:name,:stars,:comment)
+	end
 end
